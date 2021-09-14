30b879656202ec7d07ea016407b89f3125e653c8
John M. Shea <jshea@ece.ufl.edu>
2021-09-14 14:48:59 -0400

    Version 1.0 of the JupyterCards library to display animated flashcards in Jupyter and JupyterBook

---
+++ jupytercards/dynamic.py
@@ -0,0 +1,122 @@
+
+
+from IPython.core.display import display,  HTML
+import string
+import random
+import json
+import urllib
+import pkg_resources
+
+def display_flashcards(ref):
+
+
+
+    resource_package = __name__
+    styles = "<style>\n"
+    css = pkg_resources.resource_string(resource_package, "styles.css")
+    styles += css.decode("utf-8")
+    styles += "\n</style>"
+
+    script = '<script type="text/Javascript">\n'
+    js = pkg_resources.resource_string(resource_package, "flashcards.js")
+    script += js.decode("utf-8")
+    script += "\n</script>"
+
+    #print(script)
+
+
+
+    #print(card["front"], card["back"])
+    letters = string.ascii_letters
+    div_id = ''.join(random.choice(letters) for i in range(12))
+    #div_id='ABBA'
+    # print(div_id)
+
+    # Container
+    #mydiv =  '<div class="flip-container" id="'+ div_id + '"></div>'   
+    mydiv =  '<div class="flip-container" id="'+ div_id + '" onclick="flip(this)"></div>'    
+
+
+
+    #Spacer
+    spacer='<div style="height:40px"></div>'
+    
+    # Next button will go here
+    nextbutton='<div class="next" id="'
+    nextbutton+=div_id+'-next" ';
+    nextbutton+='onclick="checkFlip('
+    nextbutton+="'"+div_id+"'";
+    nextbutton+=')" > Next > </div>'
+    
+    #print(nextbutton)
+    loadData = '''<script type="text/Javascript">
+    
+    '''
+
+    loadData += "cards"+div_id+"="
+
+    if type(ref) == list:
+        #print("List detected. Assuming JSON")
+        loadData += json.dumps(ref)
+        static = True
+        url = ""
+    elif type(ref) == str:
+        if ref.lower().find("http") == 0:
+            url = ref
+            file = urllib.request.urlopen(url)
+            for line in file:
+                loadData += line.decode("utf-8")
+            static = False
+        else:
+            #print("File detected")
+            with open(ref) as file:
+                for line in file:
+                    loadData += line
+            static = True
+            url = ""
+    else:
+        raise Exception("First argument must be list (JSON), URL, or file ref")
+
+    loadData += ''';
+    '''
+    
+    if static:
+        loadData += '''
+        createCards("'''
+        loadData+= div_id  + '");'
+        loadData+='''
+        </script>
+        '''
+
+        print()
+    else:
+        loadData += '''
+
+        {
+        const jmscontroller = new AbortController();
+        const signal = jmscontroller.signal;
+
+        setTimeout(() => jmscontroller.abort(), 5000);
+
+        fetch("'''
+        script_end = '''", {signal})
+        .then(response => response.json())
+        .then(json => createCards("'''
+        # .then(json => console.log( '''
+
+        script_end += div_id
+        script_end += '''"))
+        .catch(err => {
+        console.log("Fetch error or timeout");
+        createCards("'''
+        script_end += div_id+'"'
+        script_end += ''');
+        });
+        }
+        </script>
+        '''
+        loadData+=url+script_end
+
+
+    #print(loadData)
+    display(HTML(styles+script+loadData+spacer+mydiv+spacer+nextbutton+spacer))
---
+++ jupytercards/flashcards.js
@@ -0,0 +1,192 @@
+
+function jaxify(string) {
+    var mystring = string;
+
+    count = 0;
+    var loc = mystring.search(/([^\\]|^)(\$)/);
+
+    count2 = 0;
+    var loc2 = mystring.search(/([^\\]|^)(\$\$)/);
+
+    //console.log(loc);
+
+    while ((loc >= 0) || (loc2 >= 0)) {
+
+        /* Have to replace all the double $$ first with current implementation */
+        if (loc2 >= 0) {
+            if (count2 % 2 == 0) {
+                mystring = mystring.replace(/([^\\]|^)(\$\$)/, "$1\\[");
+            } else {
+                mystring = mystring.replace(/([^\\]|^)(\$\$)/, "$1\\]");
+            }
+            count2++;
+        } else {
+            if (count % 2 == 0) {
+                mystring = mystring.replace(/([^\\]|^)(\$)/, "$1\\(");
+            } else {
+                mystring = mystring.replace(/([^\\]|^)(\$)/, "$1\\)");
+            }
+            count++;
+        }
+        loc = mystring.search(/([^\\]|^)(\$)/);
+        loc2 = mystring.search(/([^\\]|^)(\$\$)/);
+        //console.log(mystring,", loc:",loc,", loc2:",loc2);
+    }
+
+    //console.log(mystring);
+    return mystring;
+}
+
+function flip(ths) {
+    console.log(ths);
+    console.log(ths.id);
+    ths.classList.toggle("flip"); 
+    var next=document.getElementById(ths.id+'-next');
+    next.style.pointerEvents='none';
+    next.classList.add('flipped');
+    setTimeout(reenableNext, 700, next);
+}
+
+function reenableNext(next) {
+    next.style.pointerEvents='auto';
+}
+
+
+
+function slide2(containerId) {
+    var container = document.getElementById(containerId);
+    var next=document.getElementById(containerId+'-next');
+    var frontcard = container.children[0];
+    var backcard = container.children[1];
+    container.style.pointerEvents='none';
+    //backcard.style.pointerEvents='none';
+    next.style.pointerEvents='none';
+    next.classList.remove('flipped');
+    next.classList.add('hide');
+
+    //container.classList.add("prepare");
+    
+    container.className="flip-container slide";
+    backcard.parentElement.removeChild(frontcard);
+    backcard.parentElement.appendChild(frontcard);
+    setTimeout(slideback, 600, container, frontcard, backcard, next);
+    
+}
+
+
+function checkFlip(containerId) {
+    var container = document.getElementById(containerId);
+
+
+    if (container.classList.contains('flip')) {
+        container.classList.remove('flip');
+        setTimeout(slide2, 600, containerId);
+    } 
+    else {
+        slide2(containerId);
+    }
+}
+
+
+function slideback(container, frontcard, backcard, next) {
+    container.className="flip-container slideback";
+    setTimeout(cleanup, 600, container, frontcard, backcard, next);
+}
+
+function cleanup(container, frontcard, backcard, next) {
+    container.removeChild(frontcard);
+    backcard.className="flipper frontcard";
+    container.className="flip-container";
+
+    var cardnum=container.dataset.cardnum;
+    var cards=eval('cards'+container.id);
+    var flipper=createOneCard(container, false, cards, cardnum);
+    container.append(flipper);
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    container.dataset.cardnum=(cardnum+1) % container.dataset.numCards;
+    next.style.pointerEvents='auto';
+    container.style.pointerEvents='auto';
+    next.classList.remove('hide');
+
+
+}
+
+
+function createOneCard  (mydiv, frontCard, cards, cardnum) {
+    colors=[
+        '--asparagus',
+        '--terra-cotta',
+        '--cyan-process'
+    ]
+
+    var flipper = document.createElement('div');
+    if (frontCard){
+        flipper.className="flipper frontcard";    
+    }
+    else {
+        flipper.className="flipper backcard";   
+    }
+
+    var front = document.createElement('div');
+    front.className='front card';
+
+    var frontSpan= document.createElement('span');
+    frontSpan.className='cardtext';
+    frontSpan.textContent=jaxify(cards[cardnum]['front']);
+    front.style.background='var(' + colors[cardnum % colors.length] + ')';
+
+
+    front.append(frontSpan);
+
+    flipper.append(front);
+
+    var back = document.createElement('div');
+    back.className='back card';
+
+    var backSpan= document.createElement('span');
+    backSpan.className='cardtext';
+    backSpan.textContent=jaxify(cards[cardnum]['back']);
+    back.append(backSpan);
+
+    flipper.append(back);
+
+    return flipper;
+
+}
+
+
+
+
+
+function createCards(id) {
+    console.log(id);
+    
+    var mydiv=document.getElementById(id);
+    
+    var cards=eval('cards'+id);
+    mydiv.dataset.cardnum=0;
+    mydiv.dataset.numCards=cards.length;
+
+    var cardnum=0;
+    
+    for (var i=0; i<2; i++) {
+    
+        var flipper;
+        if (i==0){
+            flipper=createOneCard(mydiv, true, cards, cardnum);
+        }
+        else {
+            flipper=createOneCard(mydiv, false, cards, cardnum);
+        }
+
+        mydiv.append(flipper);
+        cardnum = (cardnum + 1) % mydiv.dataset.numCards;
+    }
+    mydiv.dataset.cardnum = cardnum;
+    
+    return flipper;
+}
+
+
+
+
