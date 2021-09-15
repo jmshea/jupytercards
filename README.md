942a272dbe1cf6d242228e503b0dafa04f47274b
John M. Shea <jshea@ece.ufl.edu>
2021-09-15 16:13:34 -0400

    Add Python program to extract definitions from Jupyter notebooks, when the definitions follow a specific format. Provided as reference for others who might want to do something similar.

--- README.md
+++ README.md
@@ -14,3 +14,16 @@ JupyterCards is available via pip:
 
 Although using JSON objects for each flashcard is overkill, this model was
 chosen to support future extensions to this library.
+
+## Example of generating flashcard files from JupyterBook notebooks
+
+In my Jupyter notebooks that are used as input to JupyterBook, I use panels with
+the heading "DEFINITION" to call out definitions in the text. I have provided a
+helper program `extractdefinitions.py` that I use to scan files for the
+DEFINITION header and extract the appropriate lines that follow. The resulting
+terms and definitions are dumped to corresponding JSON files in a "flashcards"
+directory. An additional JSON file is generated for the whole chapter.
+`extractdefinitions.py` takes 2 arguments: the directory to parse and the
+chapter number to use to label the overall JSON definitions file. **This program
+is very specific to my workflow and I am offering it only as reference in case
+it can help someone else with a similar situation.**
---
+++ extractdefinitions.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+import os
+import re
+import json
+import sys
+
+def extract_defns(directory, chapter):
+    pattern = re.compile("DEFIN")
+    defns=[]
+    chdefns=[]
+    fileroot=""
+    flashcards=directory+'/flashcards/'
+    print(flashcards)
+
+
+    print("scanning directory: ", directory)
+    for filename in os.listdir(directory):
+        if filename.endswith(".ipynb"):
+
+                
+            with open(filename,'r') as file:
+               
+                if len(defns)>0:
+                    print(fileroot, len(defns))
+                    print(defns)
+                    print("\n\n")
+                    #return defns
+                    if not os.path.isdir(flashcards):
+                        os.makedirs(flashcards)
+                    outfile=flashcards+fileroot+'.json'
+                    print(outfile)
+                    with open(outfile,'w') as out:
+                        json.dump(defns,out,indent=4)
+
+                fileroot=filename.split('.')[0]
+                #print(fileroot)
+                defns=[]
+                defn_start=0
+                for line in file:
+                    for match in re.finditer(pattern, line):
+                        defn_start=1
+
+                    if defn_start>0:
+                        if defn_start==3:
+                            term=line.replace('",','')
+                            term=term.replace('"','')
+
+
+                            term=term.strip()
+                            term=term.replace("\\n","")
+                            #print(term)
+                        if defn_start==4:
+                            defn=line.replace('",','')
+                            defn=defn.replace('"','')
+                            defn=defn.replace(':','')
+                            defn=defn.strip()
+                            defn=defn.replace("\\n","")
+                            defns+=[{"front":term, "back":defn}]
+                            chdefns+=[{"front":term, "back":defn}]
+                            
+                        
+                        #print(defn_start, line)
+                        defn_start+=1
+                    if defn_start==5:
+                        #print(term,defn)
+                        defn_start=0
+                        
+            
+        else:
+            continue
+
+    outfile='ch'+str(chapter)+'.json'
+    print(outfile)
+    with open(outfile,'w') as out:
+        json.dump(chdefns,out,indent=4)
+
+def main(args):
+	print(args)
+	if len(args)==2:
+		extract_defns( args[0], int(args[1]) )
+	else:
+		print("FAILED: expect exactly 2 arguments (directory and chapter number)")
+
+	
+if __name__ == "__main__":
+	main(sys.argv[1:])
